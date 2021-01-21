78a357418e67d4e3809cdc129d560fe94e90ed26
Vishwajit123 <vishwajit20bhagat@gmail.com>
2021-01-21 21:34:36 +0530

    project network

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Vpb123
+- LIVE: https://leo-network.herokuapp.com/
+- YOUTUBE: https://www.youtube.com/watch?v=8-V2YDcNa1U
--- mail/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User, Email;
-class EmailAdmin(admin.ModelAdmin):
-    list_display=['user','sender','subject','body','timestamp']
-admin.site.register(User)
-admin.site.register(Email,EmailAdmin)
